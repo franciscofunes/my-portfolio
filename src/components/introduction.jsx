40ba8e516fc5d33fae0d2625a28b81ab5c46d02b
franciscofunes <ffunes90@gmail.com>
2021-06-13 19:36:18 -0300

    change drive link cv

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -1,61 +1,104 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class Introduction extends Component {
-  render() {
-    return (
-      <div>
-        <section id="colorlib-hero" className="js-fullheight" data-section="home">
-          <div className="flexslider js-fullheight">
-            <ul className="slides">
-              <li style={{backgroundImage: 'url(images/img_bg.jpg)'}}>
-                <div className="overlay" />
-                <div className="container-fluid">
-                  <div className="row">
-                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
-                      <div className="slider-text-inner js-fullheight">
-                        <div className="desc">
-                          <h1>¡Bienvenidos! <br /><span role="img" aria-label="hand">👇 Ver CV </span></h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://drive.google.com/file/d/1TNPhtNajcQaw5afrFUOI0voucJoSz0jU/view?usp=sharing" target="_blank" rel="noopener noreferrer">Mi CV<i className="icon-download4" /></a></p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </li>
-              <li style={{backgroundImage: 'url(images/img_bg.jpg)'}}>
-                <div className="overlay" />
-                <div className="container-fluid">
-                  <div className="row">
-                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
-                      <div className="slider-text-inner">
-                        <div className="desc">
-                          <h1>Me encanta desarrollar y diseñar<br /> Webs !!</h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://github.com/franciscofunes" target="_blank" rel="noopener noreferrer">Mis proyectos <i className="icon-briefcase3" /></a></p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </li>
-              <li style={{backgroundImage: 'url(images/img_bg.jpg)'}}>
-                <div className="overlay" />
-                <div className="container-fluid">
-                  <div className="row">
-                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
-                      <div className="slider-text-inner">
-                        <div className="desc">
-                          <h1>También publico contenido en <br/>Linkedin ... </h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://www.linkedin.com/in/francisco-funes/" target="_blank" rel="noopener noreferrer">mirar mi perfil <i className="icon-book" /></a></p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </section>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div>
+				<section
+					id='colorlib-hero'
+					className='js-fullheight'
+					data-section='home'
+				>
+					<div className='flexslider js-fullheight'>
+						<ul className='slides'>
+							<li style={{ backgroundImage: 'url(images/img_bg.jpg)' }}>
+								<div className='overlay' />
+								<div className='container-fluid'>
+									<div className='row'>
+										<div className='col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text'>
+											<div className='slider-text-inner js-fullheight'>
+												<div className='desc'>
+													<h1>
+														¡Bienvenidos! <br />
+														<span role='img' aria-label='hand'>
+															👇 Ver CV{' '}
+														</span>
+													</h1>
+													<p>
+														<a
+															className='btn btn-primary btn-learn'
+															href='https://drive.google.com/file/d/1Sc8wPEhvisHrLkCdRqccq916MpK53g62/view?usp=sharing'
+															target='_blank'
+															rel='noopener noreferrer'
+														>
+															Mi CV
+															<i className='icon-download4' />
+														</a>
+													</p>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</li>
+							<li style={{ backgroundImage: 'url(images/img_bg.jpg)' }}>
+								<div className='overlay' />
+								<div className='container-fluid'>
+									<div className='row'>
+										<div className='col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text'>
+											<div className='slider-text-inner'>
+												<div className='desc'>
+													<h1>
+														Me encanta desarrollar y diseñar
+														<br /> Webs !!
+													</h1>
+													<p>
+														<a
+															className='btn btn-primary btn-learn'
+															href='https://github.com/franciscofunes'
+															target='_blank'
+															rel='noopener noreferrer'
+														>
+															Mis proyectos <i className='icon-briefcase3' />
+														</a>
+													</p>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</li>
+							<li style={{ backgroundImage: 'url(images/img_bg.jpg)' }}>
+								<div className='overlay' />
+								<div className='container-fluid'>
+									<div className='row'>
+										<div className='col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text'>
+											<div className='slider-text-inner'>
+												<div className='desc'>
+													<h1>
+														También publico contenido en <br />
+														Linkedin ...{' '}
+													</h1>
+													<p>
+														<a
+															className='btn btn-primary btn-learn'
+															href='https://www.linkedin.com/in/francisco-funes/'
+															target='_blank'
+															rel='noopener noreferrer'
+														>
+															mirar mi perfil <i className='icon-book' />
+														</a>
+													</p>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</li>
+						</ul>
+					</div>
+				</section>
+			</div>
+		);
+	}
 }
